d7c72585b70a3e7c6d1416eb97b0bb6b9de0db92
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-09-10 18:02:36 -0300

    feat: creating the first unit tests

---
+++ README.md
@@ -0,0 +1,3 @@
+Hoje o meu problema é o seguinte:
+
+Os meus CLIENTES prevcisam realizar AGENDAMENTOS só que eles não conseguem saber os HORÁRIOS disponíveis.
---
+++ src/entities/appointment.ts
@@ -0,0 +1,34 @@
+export interface AppointmentProps {
+  customer: string;
+  startsAt: Date;
+  endsAt: Date;
+}
+
+export class Appointment {
+  private props: AppointmentProps;
+
+  public get customer(): string {
+    return this.props.customer;
+  }
+
+  public get startsAt(): Date {
+    return this.props.startsAt;
+  }
+
+  public get endsAt(): Date {
+    return this.props.endsAt;
+  }
+
+  constructor(props: AppointmentProps) {
+    const { startsAt, endsAt } = props;
+
+    if (startsAt <= new Date()) {
+      throw new Error('Invalid start date');
+    }
+
+    if (endsAt <= startsAt) {
+      throw new Error('Invalid end date');
+    }
+    this.props = props;
+  }
+}
